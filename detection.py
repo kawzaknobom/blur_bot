c9f85f1263157e9a4cb943cfbbd2fe0871a9be86
kawzaknobom <142733725+kawzaknobom@users.noreply.github.com>
2026-01-22 19:45:48 +0200

    Update detection.py

--- detection.py
+++ detection.py
@@ -12,7 +12,7 @@ _model_loader_instance = None
 
 from huggingface_hub import hf_hub_download
 
-sam_vit_h_4b8939 = hf_hub_download(repo_id="HCMUE-Research/SAM-vit-h", filename="sam_vit_h_4b8939")
+sam_vit_h_4b8939 = hf_hub_download(repo_id="HCMUE-Research/SAM-vit-h", filename="sam_vit_h_4b8939.pth")
 yolov8x = hf_hub_download(repo_id="Ultralytics/YOLOv8", filename="yolov8x.pt")
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -184,3 +184,4 @@ demo = gr.Interface(
 if __name__ == "__main__":
 
     demo.launch(server_name="localhost", server_port=8413)
+
